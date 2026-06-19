8ad72fda355a65874443f783b7638d755a367938
Mikkel Kronborg <mkronborg7@gmail.com>
2026-06-19 23:47:16 +0200

    move the constucter

---
+++ src/file.cpp
@@ -0,0 +1,34 @@
+
+#include "file.hpp"
+
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <ostream>
+#include <stdexcept>
+#include <string>
+
+File::File(std::string d) {
+  std::cout << d << std::endl;
+  if (d.empty())
+    throw std::invalid_argument("dir cannot be emtpy");
+
+  char buffer[80];
+  dir = d;
+
+  auto now = std::chrono::system_clock::now();
+  std::time_t t = std::chrono::system_clock::to_time_t(now);
+
+  std::tm tm = *std::localtime(&t);
+
+  std::strftime(buffer, sizeof(buffer), "log %Y-%m-%d %H-%M-%S.txt", &tm);
+
+  std::ofstream outFile(dir + buffer);
+
+  if (!outFile)
+    throw std::runtime_error(
+        "failed to create file, make sure the path is cronect");
+
+  name = buffer;
+};
